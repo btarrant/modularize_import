175a04a87fc64150eeb312e4da83cb7184330ded
Brandon Tarrant <brandontarrant04@gmail.com>
2022-03-01 20:06:55 -0500

    Implemented the init functions of the classes.

--- dojo_pets.py
+++ dojo_pets.py
@@ -1,20 +1,33 @@
 class Ninja:
+                    # implement __init__( first_name , last_name , treats , pet_food , pet )
 
-            # implement __init__( first_name , last_name , treats , pet_food , pet )
+
+def __init__(self, first_name, last_name, treats, pet_food, pet):
+    self.first_name = first_name
+    self.last_name = last_name
+    self.treats = treats
+    self.pet_food = pet_food
+    self.pet = pet
 
 
-        # implement the following methods:
-        # walk() - walks the ninja's pet invoking the pet play() method
-        # feed() - feeds the ninja's pet invoking the pet eat() method
-        # bathe() - cleans the ninja's pet invoking the pet noise() method
+                # implement the following methods:
+                # walk() - walks the ninja's pet invoking the pet play() method
+                # feed() - feeds the ninja's pet invoking the pet eat() method
+                # bathe() - cleans the ninja's pet invoking the pet noise() method
 
 
 class Pet:
+                    # implement __init__( name , type , tricks ):
 
 
-            # implement __init__( name , type , tricks ):
-        # implement the following methods:
-        # sleep() - increases the pets energy by 25
-        # eat() - increases the pet's energy by 5 & health by 10
-        # play() - increases the pet's health by 5
-        # noise() - prints out the pet's sound
+def __init__(self, name, type, tricks):
+    self.name = name
+    self.type = type
+    self.tricks = tricks
+
+
+                # implement the following methods:
+                # sleep() - increases the pets energy by 25
+                # eat() - increases the pet's energy by 5 & health by 10
+                # play() - increases the pet's health by 5
+                # noise() - prints out the pet's sound
